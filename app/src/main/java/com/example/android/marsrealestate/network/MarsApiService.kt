e773721138b269fc382c9b3c55b90e9b6ef42f2e
ikrotzky <ikr@croworc.com>
2019-06-09 08:27:29 +0200

    [Step.01-Ex] feat: Implement Retrofit `machinery` in MarsApiService.kt

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,8 +17,43 @@
 
 package com.example.android.marsrealestate.network
 
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
 private const val BASE_URL = "https://mars.udacity.com/"
 
-// TODO (02) Use Retrofit Builder with ScalarsConverterFactory and BASE_URL
-// TODO (03) Implement the MarsApiService interface with @GET getProperties returning a String
-// TODO (04) Create the MarsApi object using Retrofit to implement the MarsApiService
+// COMPLETED (02) Use Retrofit Builder with ScalarsConverterFactory and BASE_URL
+private val retrofit = Retrofit.Builder()
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+
+// COMPLETED (03) Implement the MarsApiService interface with @GET getProperties returning a String
+/*
+ * Defines an interface that explains how Retrofit talks to our webserver, using HTTP GET requests.
+ * Retrofit will build an object implementing this interface with all of the methods that talk to
+ * the server, conceptually much like Room implements our DAO interfaces to build ORM objects to
+ * talk to our SQLite database.
+ */
+interface MarsApiService {
+    @GET("realestate") // the endpoint for the JSON response this method is gonna use
+    fun getProperties():
+            Call<String> // The returned call object is used to start the request
+}
+
+// COMPLETED (04) Create the MarsApi object using Retrofit to implement the MarsApiService
+/*
+* To create a Retrofit service, we call retrofit.create(...), passing in the service interface we
+* just defined.
+* Since this retrofit.create(...) call is expensive, and our app just needs one retrofit service
+* instance, we'll expose our Retrofit service to the rest of the application using a public object
+* called `MarsApi`.
+* Calling `MarsApi.retrofitService` will thus return a Retrofit object that implements MarsApiService.
+*/
+object MarsAPI {
+    val retrofitService: MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+}
