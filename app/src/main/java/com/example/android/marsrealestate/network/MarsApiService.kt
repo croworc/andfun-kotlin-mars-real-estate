2bb5cd8d61b90e7254bda9db118b23647b82045c
ikrotzky <ikr@croworc.com>
2020-08-15 20:07:45 +0200

    [Step.08-Ex] feat: Add enum class for property type filter
    Also: Add query parameter for filter string in
    getPropertiesAsync()

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -24,9 +24,11 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import kotlinx.coroutines.Deferred
+import retrofit2.http.Query
 
 private const val BASE_URL = "https://mars.udacity.com/"
-// TODO (01) Create an enum full of constants to match the query values our web service expects
+// COMPLETED (01) Create an enum full of constants to match the query values our web service expects
+enum class MarsApiFilter(val value: String) { SHOW_RENT("rent"), SHOW_BUY("buy"), SHOW_ALL("all") }
 
 /**
  * Build the Moshi object that Retrofit will be using, making sure to add the Kotlin adapter for
@@ -47,7 +49,7 @@ private val retrofit = Retrofit.Builder()
         .build()
 
 /**
- * A public interface that exposes the [getProperties] method
+ * A public interface that exposes the [getPropertiesAsync] method
  */
 interface MarsApiService {
     /**
@@ -56,9 +58,9 @@ interface MarsApiService {
      * The @GET annotation indicates that the "realestate" endpoint will be requested with the GET
      * HTTP method
      */
-    // TODO (02) Add filter @Query value to the getProperties method
+    // COMPLETED (02) Add filter @Query value to the getProperties method
     @GET("realestate")
-    fun getProperties():
+    fun getPropertiesAsync(@Query("filter") type: String):
     // The Coroutine Call Adapter allows us to return a Deferred, a Job with a result
             Deferred<List<MarsProperty>>
 }
